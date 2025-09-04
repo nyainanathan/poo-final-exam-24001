90c19926cbb731f89afce06e6e12895e097f2709
Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2025-09-04 10:34:11 +0300

    feat: get fee status method

--- src/main/java/Frais.java
+++ src/main/java/Frais.java
@@ -1,11 +1,35 @@
 import lombok.AllArgsConstructor;
 
 import java.time.Instant;
+import java.util.List;
 
 @AllArgsConstructor
 public class Frais {
+    public enum FraisStatus{
+        IN_PROGRESS,
+        PAID,
+        LATE,
+        OVERPAID
+    }
     private final String id;
     private final String label;
     private final double montant;
     private final Instant deadline;
+    private List<Paiement> paiementsAssocies;
+
+    public FraisStatus getFraisStatus(){
+        double totalPaiements= paiementsAssocies.stream()
+                .mapToDouble(Paiement::getMontant)
+                .sum();
+        if(this.deadline.isAfter(Instant.now()) && totalPaiements < this.montant){
+            return FraisStatus.IN_PROGRESS;
+        } else if (totalPaiements == this.montant){
+            return FraisStatus.PAID;
+        } else if (this.deadline.isBefore(Instant.now()) && totalPaiements < this.montant){
+            return FraisStatus.LATE;
+        } else if (totalPaiements >= this.montant){
+            return FraisStatus.OVERPAID;
+        }
+        return FraisStatus.IN_PROGRESS;
+    }
 }
--- src/main/java/Paiement.java
+++ src/main/java/Paiement.java
@@ -1,8 +1,10 @@
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.time.Instant;
 
 @AllArgsConstructor
+@Getter
 public class Paiement {
     private final int id;
     private final double montant;
